5cc4ff3880aca4aa14f5cf2d4bea6fed80da3f56
joaooliveira-11 <up202108737@edu.fe.up.pt>
2023-10-19 01:28:45 +0100

    database schema

--- SQL/database.sql
+++ SQL/database.sql
@@ -1,22 +1,30 @@
-DROP TABLE IF EXISTS User CASCADE;
-DROP TABLE IF EXISTS Interest CASCADE;
-DROP TABLE IF EXISTS UserInterests CASCADE;
-DROP TABLE IF EXISTS Skill CASCADE;
-DROP TABLE IF EXISTS UserSkills CASCADE;
-DROP TABLE IF EXISTS Project CASCADE;
-DROP TABLE IF EXISTS ProjectUsers CASCADE;
-DROP TABLE IF EXISTS FavoriteProjects CASCADE;
-DROP TABLE IF EXISTS Invite CASCADE;
-DROP TABLE IF EXISTS Task CASCADE;
-DROP TABLE IF EXISTS Comment CASCADE;
+create schema if not exists lbaw23117;
+SET search_path TO lbaw23117;
+
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS interest CASCADE;
+DROP TABLE IF EXISTS user_interests CASCADE;
+DROP TABLE IF EXISTS skill CASCADE;
+DROP TABLE IF EXISTS user_skills CASCADE;
+DROP TABLE IF EXISTS project CASCADE;
+DROP TABLE IF EXISTS project_users CASCADE;
+DROP TABLE IF EXISTS favorite_projects CASCADE;
+DROP TABLE IF EXISTS invite CASCADE;
+DROP TABLE IF EXISTS task CASCADE;
+DROP TABLE IF EXISTS comment CASCADE;
+DROP TABLE IF EXISTS message CASCADE;
+DROP TABLE IF EXISTS comment CASCADE;
+DROP TABLE IF EXISTS notification CASCADE;
+
 
 DROP TYPE IF EXISTS statusTypes;
 DROP TYPE IF EXISTS editedTypes;
 
 CREATE TYPE statusTypes AS ENUM ('open', 'closed', 'archived');
 CREATE TYPE edited AS ENUM ('original', 'edited');
+CREATE TYPE notificationType as ENUM('assignedtask, archivedtask, invite, forum, comment, acceptedinvite');
 
-CREATE TABLE User (
+CREATE TABLE users (
     userId INTEGER PRIMARY KEY,
     name TEXT NOT NULL,
     username TEXT NOT NULL UNIQUE,
@@ -29,64 +37,62 @@ CREATE TABLE User (
     emailVerification BOOLEAN NOT NULL DEFAULT FALSE 
 );
 
-CREATE TABLE Interest (
+CREATE TABLE interest (
     interestId INTEGER PRIMARY KEY,
     interest TEXT NOT NULL 
 );
 
-CREATE TABLE UserInterests(
-    FOREIGN KEY serId INTEGER REFERENCES user,
-    interestId INTEGER REFERENCES interest,
-    PRIMARY KEY (userId, Id)
+CREATE TABLE user_interests(
+    userId INTEGER REFERENCES Users,
+    interestId INTEGER REFERENCES Interest,
+    PRIMARY KEY (userId, interestId)
 );
 
-CREATE TABLE Skill (
+CREATE TABLE skill (
     skillId INTEGER PRIMARY KEY,
     skill TEXT NOT NULL 
 );
 
-CREATE TABLE UserSkills(
-    userId INTEGER REFERENCES user,
-    skillId INTEGER REFERENCES skill,
+CREATE TABLE user_skills(
+    userId INTEGER REFERENCES Users,
+    skillId INTEGER REFERENCES Skill,
     PRIMARY KEY (userId, skillId)
 ); 
 
-CREATE TABLE Project (
+CREATE TABLE project (
     projectId INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
     description TEXT NOT NULL,
     isPublic BOOLEAN NOT NULL DEFAULT TRUE,
     archived BOOLEAN NOT NULL DEFAULT FALSE,
     createDate TIMESTAMP NOT NULL CHECK(createDate <= now()),
-    isPublic BOOLEAN NOT NULL DEFAULT TRUE,
-    archived BOOLEAN NOT NULL DEFAULT FALSE,
-    createdBy INTEGER NOT NULL REFERENCES user,
-    projectCoordinator NOT NULL REFERENCES user
+    createdBy INTEGER NOT NULL REFERENCES Users,
+    projectCoordinator INTEGER NOT NULL REFERENCES Users
 );
 
-CREATE TABLE ProjectUsers (
-    projectId INTEGER REFERENCES project,
-    userId INTEGER REFERENCES user,
+CREATE TABLE project_users (
+    projectId INTEGER REFERENCES Project,
+    userId INTEGER REFERENCES Users,
     PRIMARY KEY (projectId, userId)
 );
 
-CREATE TABLE FavoriteProjects (
-    projectId INTEGER REFERENCES project,
-    userId INTEGER REFERENCES user,
+CREATE TABLE favorite_projects (
+    projectId INTEGER REFERENCES Project,
+    userId INTEGER REFERENCES Users,
     PRIMARY KEY (projectId, userId)
 );
 
-CREATE TABLE Invite (
+CREATE TABLE invite (
     inviteId INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
     description TEXT NOT NULL,
     createDate TIMESTAMP NOT NULL CHECK (createDate <= now()),
-    invitedBy INTEGER NOT NULL REFERENCES User,
-    invitedTo INTEGER NOT NULL REFERENCES User,
+    invitedBy INTEGER NOT NULL REFERENCES Users,
+    invitedTo INTEGER NOT NULL REFERENCES Users,
     projectInvite INTEGER NOT NULL REFERENCES Project
 );
 
-CREATE TABLE Task (
+CREATE TABLE task (
     taskId INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
     description TEXT NOT NULL,
@@ -94,37 +100,36 @@ CREATE TABLE Task (
     createDate TIMESTAMP NOT NULL CHECK(createDate <= now()),
     finishDate TIMESTAMP,
     state statusTypes NOT NULL DEFAULT 'open',
-    createBy INTEGER NOT NULL REFERENCES User,
-    assignedTo INTEGER REFERENCES User,
+    createBy INTEGER NOT NULL REFERENCES Users,
+    assignedTo INTEGER REFERENCES Users,
     projectTask INTEGER REFERENCES Project 
 );
     
-CREATE TABLE Comment (
+CREATE TABLE comment (
     commentId INTEGER PRIMARY KEY,
     content TEXT NOT NULL,
     createDate TIMESTAMP NOT NULL CHECK (createDate <= now()),
     edited BOOLEAN NOT NULL DEFAULT FALSE,
-    commentBy INTEGER NOT NULL REFERENCES User,
+    commentBy INTEGER NOT NULL REFERENCES Users,
     taskComment INTEGER NOT NULL REFERENCES Task   
 );
 
-CREATE TABLE Message (
+CREATE TABLE message (
     messageId INTEGER PRIMARY KEY,
     content TEXT NOT NULL,
     createDate TIMESTAMP NOT NULL CHECK (createDate <= now()),
     edited BOOLEAN NOT NULL DEFAULT FALSE,
-    messageBy INTEGER REFERENCES User,
+    messageBy INTEGER REFERENCES Users,
     projectMessage INTEGER REFERENCES Project
 );
 
-CREATE TABLE Notification (
+CREATE TABLE notification (
     notificationId INTEGER PRIMARY KEY,
     createDate TIMESTAMP NOT NULL CHECK (createDate <= now()),
     viewed BOOLEAN NOT NULL DEFAULT FALSE,
-    emitedBy INTEGER NOT NULL REFERENCES User, 
-    emitedTo INTEGER NOT NULL REFERENCES User
+    emitedBy INTEGER NOT NULL REFERENCES Users, 
+    emitedTo INTEGER NOT NULL REFERENCES Users,
+    type notificationType NOT NULL,
+    referenceID INTEGER NOT NULL
 );
-
-
-
 
