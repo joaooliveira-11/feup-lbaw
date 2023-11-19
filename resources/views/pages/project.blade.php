1f3d00042dabb05d8ef98acf6b10a5f6c4387ba5
inessalexandre <up202103343@up.pt>
2023-11-19 17:26:11 +0000

    project page frontend

--- resources/views/pages/project.blade.php
+++ resources/views/pages/project.blade.php
@@ -7,34 +7,42 @@
     <div id="ProjectInfo">
         <div id="Sidebar">
             <h1>{{ $project->title }}</h1> 
+            <div class="title-line"></div>
             <a href="">Dashboard</a>
-            <a href="">Messages</a>
+            <a href="">Chat</a>
             <a href=" ">Tasks</a>
             <a href="">Members</a>
+            <a href="#" id="leaveProject" class="center-button">Leave Project</a>
         </div>
+
         <div id="MainContent">
-            <div id="Name">
-                <p>{{ $project->title }}</p>
+            <div id="ProjectDeadline">
+                <p id="TitleInPage">Project Deadline:</p>
+                <p>{{ $project->deadline !== null ? $project->deadline : 'Not defined' }}</p>
+                <button id="CreateTaskButton">Create Task</button>
+                <button id="CreateTaskButton">Invite User</button>
             </div>
-            <div id="Description">
+            <div id="ProjectDescription">
+                <p id="TitleInPage">Details:</p>
                 <p>{{ $project->description }}</p>
             </div>
-        
-            <div id="Projects">
-                <p class="label">Tasks</p>
+            <div id="Tasks">
+                <div class="line-before-tasks"></div>
+                <p class="TaskLabel">Tasks:</p>
                 @if($project->tasks->count() > 0)
-                    <ul id="ProjectsList">
+                    <ul id="TasksList">
                         @foreach($project->tasks as $task)
                             <li>
                                 <div>
-                                    <p id="ProjectTitle">{{ $task->name }}</p>
+                                    <p id="TaskTitle">{{ $task->title }}</p>
                                     <p>{{ $task->description }}</p>
+                                    <p class="FinishDate">Deadline: {{ $task->finish_date !== null ? $task->finish_date : 'Not defined' }}</p>
                                 </div>
                             </li> 
                         @endforeach
                     </ul>
                 @else
-                    <p>No tasks found for this project.</p>
+                <p>No tasks found for this project.</p>
                 @endif
             </div>
         </div>
