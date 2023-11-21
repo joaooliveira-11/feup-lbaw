a95cb5bde1980f8b6defc8b3f5596765c708df62
samuel <up202108751@edu.fe.up.pt>
2023-11-21 11:58:08 +0000

    Auth Pages Fixed

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,6 +4,7 @@
 <div id="login_page_content">
   <div id=login_background>
       <img src="{{ url('/img/login_background.png') }}" id="main_image"/>
+      <a class="button button-outline" href="{{ route('login') }}">Login</a>
   </div>
   <div id="signin_content">
       <h1>Register Your Account</h1>
@@ -52,10 +53,12 @@
             </div>
 
           <div id="butons_signin">
-              <button type="submit">Register</button>
+              <button type="submit" id="RegisterButton">Register</button>
           </div>
-          <a class="button button-outline" id="button-login" href="{{ route('login') }}">Login</a>
         </form>
+        <div id="register-mobile">
+            <span class="span-space">Already have an account?</span><a id="register-mobile-link" href="{{ route('login') }}">Log In</a>
+        </div>
   </div>
 </div>
 @endsection
