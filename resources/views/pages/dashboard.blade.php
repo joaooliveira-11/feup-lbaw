d43ccd63b515796e08a8b1e5dcbde8d7be217e57
inessalexandre <up202103343@up.pt>
2023-12-19 21:28:18 +0000

    admin can search users

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -2,31 +2,36 @@
 
 @section('content')
 
-<div class="row">
-    <div class="col-md-12">
-        <a href="{{ url('/project/create') }}" class="btn btn-primary float-right mb-3">Criar Projeto</a>
-    </div>
-    <div class="col-md-8 mx-auto">
-        <table class="table table-hover table-bordered">
-            <thead class="thead-dark">
-                <tr>
-                    <th>ID</th>
-                    <th>Nome</th>
-                    <th>Email</th>
-                    <!-- Outros campos que deseja exibir -->
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($users as $user)
+<div class="dashboardPage">
+    <div class="row">
+        <div class="col-md-8 mx-auto mt-6">
+            <h4 class="mb-3">Search Users:</h4>
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Search for users...">
+                <div class="input-group-append">
+                    <button class="btn btn-outline-secondary" type="button">Search</button>
+                </div>
+            </div>
+
+            <table class="table table-hover table-bordered custom-table">
+                <thead class="thead-dark">
                     <tr>
-                        <td>{{ $user->id }}</td>
-                        <td>{{ $user->name }}</td>
-                        <td>{{ $user->email }}</td>
-                        <!-- Outros campos do usuário -->
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Email</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach($users as $user)
+                        <tr onclick="window.location='{{ url('/profile/' . $user->id) }}';" style="cursor:pointer;">
+                            <td>{{ $user->id }}</td>
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->email }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
 
