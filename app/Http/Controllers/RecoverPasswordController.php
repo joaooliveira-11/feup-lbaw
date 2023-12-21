877a2b04d4db839b36baabb88a7a5010d22ca3cd
inessalexandre <up202103343@up.pt>
2023-12-21 17:19:28 +0000

    fixed recover password issue

--- app/Http/Controllers/RecoverPasswordController.php
+++ app/Http/Controllers/RecoverPasswordController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Password;
 use RealRashid\SweetAlert\Facades\Alert;
 
 class RecoverPasswordController extends Controller {
+  
   public function show() {
     return view('auth.forgotPassword');
   }
@@ -17,16 +18,17 @@ class RecoverPasswordController extends Controller {
   public function request(Request $request) {
     $request->validate(['email' => 'required|email']);
 
-    $status = Password::sendResetLink(
-      $request->only('email')
-    );
+    $status = Password::sendResetLink($request->only('email'));
 
     if ($status === Password::RESET_LINK_SENT) {
       Alert::success('Success', 'We have e-mailed your password reset link!');
       return back();
+    } elseif ($status === Password::INVALID_USER) {
+        Alert::error('Error', 'No account found with that email address.');
+        return back();
     } else {
-      Alert::error('Error', 'Invalid credentials!');
-      return back()->withErrors(['email' => __($status)]);
+        Alert::error('Error', 'Invalid credentials!');
+        return back()->withErrors(['email' => __($status)]);
     }
   }
 
@@ -35,30 +37,33 @@ class RecoverPasswordController extends Controller {
   }
 
   public function recover(Request $request) {
-    $request->validate([
-      'token' => 'required',
-      'email' => 'required|email',
-      'password' => 'required|min:8|confirmed',
-    ]);
+    $request->validate($this->passwordResetValidationRules());
 
     $status = Password::reset(
-      $request->only('email', 'password', 'password_confirmation', 'token'),
-      function ($user, $password) use ($request) {
-        $user->forceFill([
-          'password' => Hash::make($password)
-        ]);
-
-        $user->save();
-
-        event(new PasswordReset($user));
-      }
+        $request->only('email', 'password', 'password_confirmation', 'token'),
+        function ($user, $password) {
+            $user->forceFill(['password' => Hash::make($password)])->save();
+            event(new PasswordReset($user));
+        }
     );
 
     if ($status == Password::PASSWORD_RESET) {
-      redirect()->route('login');
+        return redirect()->route('login')->with('status', 'Your password has been reset. You can now login with your new password.');
     } else {
-      Alert::error('Error', 'Invalid password!');
-      return back()->withErrors(['email' => __($status)]);
+        Alert::error('Error', 'Invalid password!');
+        return back()->withErrors(['email' => __($status)]);
     }
+  }
+
+  private function validationRules() {
+      return ['email' => 'required|email'];
+  }
+
+  private function passwordResetValidationRules() {
+      return [
+          'token' => 'required',
+          'email' => 'required|email',
+          'password' => 'required|min:8|confirmed',
+      ];
   }
 }
